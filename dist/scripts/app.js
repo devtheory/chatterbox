8ac17315908767f48874cad20948357e4d1342ff
Jose Sanchez <jparkers0012@gmail.com>
2017-01-04 12:13:30 -0500

    initial setup

---
+++ dist/scripts/app.js
@@ -0,0 +1,20 @@
+(function () {
+    function config($stateProvider, $locationProvider) {
+        $locationProvider
+            .html5Mode({
+                enabled: true, //disable the hashbang URLs
+                requireBase: false
+            });
+
+        $stateProvider
+            .state('main', {
+                url: '/',
+                controller: 'MainCtrl as main',
+                templateUrl: '/templates/main.html'
+            })
+    }
+
+    angular
+        .module('chatterbox', ['ui.router', 'firebase'])
+        .config(config);
+})();
